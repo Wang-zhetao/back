15e472ec5c49f0ffd6199a5c5e4042be7687d337
king-open <kingwang.0624@gmail.com>
2022-09-08 22:22:05 +0800

    Money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    <p>Money.vue</p>
+  <div class="nav-wrapper">
+    <div class="content">
+    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p>
+    <p>Money</p>
+    </div>
     <Nav/>
   </div>
 </template>
@@ -12,5 +23,15 @@
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper{
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content{
+  border: 1px solid blue;
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
